194bb90553560bc3ea341c3f4dada9638006e208
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-06-25 22:51:05 +0300

    update: fridge screen added

---
+++ app/home/fridge.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Link, Tabs } from "expo-router";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-green">
+      <Tabs options={{ tabBarLabel: "Fridge" }} />
+      <Text className="text-white text-lg">Fridge Screen</Text>
+    </View>
+  );
+}
