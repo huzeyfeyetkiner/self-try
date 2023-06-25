5c25d343d011296d8b8ced5eb93f30ec8e2165d9
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-06-25 22:49:53 +0300

    update: tabs navigation under home route

--- app/home/_layout.js
+++ app/home/_layout.js
@@ -2,6 +2,11 @@ import { Tabs, Stack } from "expo-router";
 
 export default function HomeLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false, headerTransparent: true }} />
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" options={{ tabBarLabel: "Home" }} />
+      <Tabs.Screen name="menu" options={{ tabBarLabel: "Menu" }} />
+      <Tabs.Screen name="fridge" options={{ tabBarLabel: "Fridge" }} />
+      <Tabs.Screen name="profile" options={{ tabBarLabel: "Profile" }} />
+    </Tabs>
   );
 }
