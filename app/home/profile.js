21647c4edb56dff97e1772ccb8a38575c152a500
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-06-27 22:32:54 +0300

    fix: profile to login route

--- app/home/profile.js
+++ app/home/profile.js
@@ -1,12 +1,11 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import { useNavigation } from "expo-router";
+import { useRouter } from "expo-router";
 function profile() {
-  const navigation = useNavigation();
-
+  const router = useRouter();
   return (
     <View className="flex-1 justify-center items-center bg-blue-500">
       <Text className="text-white text-lg">Profile Screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("/signup")}>
+      <TouchableOpacity onPress={() => router.replace("/signup")}>
         <Text>Go to Signup</Text>
       </TouchableOpacity>
     </View>
