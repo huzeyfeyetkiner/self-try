6fe1459d951da86d7fb35b4399d0f48eb792ca15
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-06-27 15:19:24 +0300

    update: minor css

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ import { NativeWindStyleSheet } from "nativewind";
 function home() {
   return (
     <View className="flex-1 justify-center items-center bg-red-500">
-      <Text style={{ color: "red" }}>Onboarding screen</Text>
+      <Text style={{ color: "white" }}>Onboarding screen</Text>
       <Link href="/signup">Route to Signup</Link>
       <Link href="/login">Route to Login</Link>
     </View>
