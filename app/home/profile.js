d0da908baf0dd50710ac7ceadfcafc8c3715eeb5
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-06-27 18:51:30 +0300

    error: from profile to auth route will fix

--- app/home/profile.js
+++ app/home/profile.js
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native";
-
+import { View, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "expo-router";
 function profile() {
+  const navigation = useNavigation();
+
   return (
     <View className="flex-1 justify-center items-center bg-blue-500">
       <Text className="text-white text-lg">Profile Screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("/signup")}>
+        <Text>Go to Signup</Text>
+      </TouchableOpacity>
     </View>
   );
 }
